3a049159258d88f9bf91e07cd6cb38c4fe90e102
niteshjan06 <niteshjan06@gmail.com>
2026-07-26 12:34:51 +0530

    singleNumber function to find unique element

---
+++ q-136.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0];
+        }
+
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            x = x^nums[i]; // taking XOR of every element.
+        }
+
+        return x;
+    }
+};
