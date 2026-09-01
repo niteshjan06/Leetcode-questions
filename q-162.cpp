af41387507697384a5e48f6d0b72be5a924423f5
niteshjan06 <niteshjan06@gmail.com>
2026-09-01 11:06:49 +0530

    Implement findPeakElement function in Solution class

---
+++ q-162.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        if(arr.size()==1){
+            return 0;
+        }
+        if(arr[0]>arr[1]){
+            return 0;
+        }
+        if(arr[arr.size()-1]>arr[arr.size()-2]){
+            return arr.size()-1;
+        }
+        int low=1;
+        int high=arr.size()-2;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return mid;
+            }
+
+            if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        
+        return -1;
+    }
+};
